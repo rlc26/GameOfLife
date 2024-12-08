02d13683c5559e5e1219fe3667c320f3549e7073
rlc26 <romane.leblondcoquaz@etu.emse.fr>
2024-12-08 15:08:09 +0100

    Set up static code analysis

--- lint_script.py
+++ lint_script.py
@@ -5,13 +5,12 @@ from flake8.main import application
 def main():
     # Ajoute les fichiers à vérifier
     sys.argv.extend(["gameoflife/life.py", "gameoflife/life_test.py"])
-    
+
     # Crée l'application Flake8
     app = application.Application()
-    
+
     # Initialisation avec argv et configuration nécessaire
     app.initialize(argv=sys.argv)
-    
+
     # Exécute le linting
     app.run_checks()
-
